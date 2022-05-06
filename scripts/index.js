fc5d66c8f00e04c6473f50a51bddb67c16bb1082
brijeshkumarrai <brijeshrai9820@gmail.com>
2022-05-06 09:58:36 +0530

    cards_box added

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,9 @@ import header from "../components/haeader.js";
 let head = document.getElementById("header")
 head.innerHTML = header();
 // document.body.innerHTML = header();
+import cardsBox from "../components/cards.js";
+let card = document.getElementById('cards_box')
+card.innerHTML = cardsBox();
 
 import footer from "../components/footer.js";
 let foot = document.getElementById("footer")
