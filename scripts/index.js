9491bda608a29f1dee71a816ae81cc587f3ca2e1
brijeshkumarrai <brijeshrai9820@gmail.com>
2022-05-06 09:52:18 +0530

    footerAdded

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,7 @@ import header from "../components/haeader.js";
 let head = document.getElementById("header")
 head.innerHTML = header();
 // document.body.innerHTML = header();
+
+import footer from "../components/footer.js";
+let foot = document.getElementById("footer")
+foot.innerHTML = footer();
