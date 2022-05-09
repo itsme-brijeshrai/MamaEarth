c51840b0771a01523ab68d076f5cf080f4461641
maheendran kp <maheendrankp100@gmail.com>
2022-05-09 16:15:38 +0530

    cart added

--- baby.js
+++ baby.js
@@ -9,7 +9,7 @@ let container1 = document.getElementById('container-1')
         {
             img: "https://honasa-mamaearth-production.imgix.net/_/m/_milky-soft-body-lotion__1.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 399.00,
-           
+           count:1,
             offer: "(5% off)",
             desc: "Milky Soft Body Lotion for Babies with Oats, Milk and Calendula - 400ml"
         },
@@ -19,7 +19,7 @@ let container1 = document.getElementById('container-1')
             img: "https://honasa-mamaearth-production.imgix.net/n/o/nourishing-bathing-bars_2_anb6wlrvagej2noa.jpg?auto=compress&fit=scale&w=400&h=400",
 
             price: 499.00,
-            
+            count:1,
             offer: "(10% off)",
             desc: "Nourishing Bathing Bar Soap For Kids - ( Pack of 5, 75g Each)"
         },
@@ -28,7 +28,7 @@ let container1 = document.getElementById('container-1')
         {
             img: "https://honasa-mamaearth-production.imgix.net/b/a/baby_hair_oil_200_ml_1200x1200__yh1i6qtlgyc253s5.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 199.00,
-          
+            count:1,
             offer: "(8% off)",
             desc: "Nourishing Hair Oil for Babies with Almond and Avocado Oil - 200 ml"
         },
@@ -36,7 +36,7 @@ let container1 = document.getElementById('container-1')
         {
             img: "https://honasa-mamaearth-production.imgix.net/f/d/fds_9573-props_qwhqvlzshodrynok.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 242.00,
-            
+            count:1,
             offer: "(12% off)",
             desc: "Moisturizing Bathing Bar Soap For Babies, pack of 2*75g"
 
@@ -89,11 +89,11 @@ let container1 = document.getElementById('container-1')
     
    
 
-    // function addCart(elem){
-    //     console.log(elem)
-    //     cart.push(elem)
-    //     localStorage.setItem('cartdata',JSON.stringify(cart))
-    // }
+    function addCart(elem){
+        console.log(elem)
+        cart.push(elem)
+        localStorage.setItem('cartdata',JSON.stringify(cart))
+    }
  
 
     // ------------------------
@@ -102,101 +102,101 @@ let container1 = document.getElementById('container-1')
     var arr2 = [
       
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/o/r/orange-body-lotions.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 239.00,
             desc: "Original Orange Body Lotion For Kids With Orange & Shea Butter - 400 ml"
         },
 
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/m/a/mango-body-lotions.jpg?auto=format&fit=crop&w=768&auto=compress",
             price: 489.00,
             desc: "Major Mango Body Lotion For Kids with Mango Butter & Milk Protein - 400 ml"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/b/r/brave-blueberry-body-lotion.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 79.00,
             desc: "Brave Blueberry Body Lotion For Kids with Blueberry and Kokum Butter– 400 ml"
         },
 
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/a/g/agent_apple-body-lotion.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 49.00,
             desc: "Agent Apple Body Lotion for Kids with Apple & Oat Milk– 400 ml"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/a/g/agent_apple-body-lotion.jpg?auto=format&fit=crop&w=768&auto=compress",
             price: 399.00,
             desc: "Agent Apple Body Lotion for Kids with Apple & Oat Milk– 400 ml"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/m/i/milky-soft-lip-balm-1.jpg?auto=format&fit=crop&w=768&auto=compress",
             price: 199.00,
             desc: "Milky Soft Natural Lip Balm for Babies with Oats, Milk & Calendula – 4g"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/m/i/milky-soft-diaper-rash-cream-1.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 299.00,
             desc: "Milky Soft Diaper Rash Cream for Babies - 50g"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/_/m/_milky-soft-body-lotion__1.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 270.00,
             desc: "Milky Soft Body Lotion for Babies with Oats, Milk and Calendula - 400ml"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/o/r/orange-body-lotions.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 360.00,
             desc: "Original Orange Body Lotion For Kids With Orange & Shea Butter - 400 ml"
         },
 
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/m/a/mango-body-lotions.jpg?auto=format&fit=crop&w=768&auto=compress",
             price: 190.00,
             desc: "Major Mango Body Lotion For Kids with Mango Butter & Milk Protein - 400 ml"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/b/r/brave-blueberry-body-lotion.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 399.00,
             desc: "Brave Blueberry Body Lotion For Kids with Blueberry and Kokum Butter– 400 ml"
         },
 
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/a/g/agent_apple-body-lotion.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 159.00,
             desc: "Agent Apple Body Lotion for Kids with Apple & Oat Milk– 400 ml"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/a/g/agent_apple-body-lotion.jpg?auto=format&fit=crop&w=768&auto=compress",
             price: 299.00,
             desc: "Agent Apple Body Lotion for Kids with Apple & Oat Milk– 400 ml"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/m/i/milky-soft-lip-balm-1.jpg?auto=format&fit=crop&w=768&auto=compress",
             price: 199.00,
             desc: "Milky Soft Natural Lip Balm for Babies with Oats, Milk & Calendula – 4g"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/m/i/milky-soft-diaper-rash-cream-1.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 299.00,
             desc: "Milky Soft Diaper Rash Cream for Babies - 50g"
         },
 
-        {
+        { count:1,
             img: "https://honasa-mamaearth-production.imgix.net/_/m/_milky-soft-body-lotion__1.jpg?auto=compress&fit=scale&w=400&h=400",
             price: 50.00,
             desc: "Milky Soft Body Lotion for Babies with Oats, Milk and Calendula - 400ml"
---
+++ scripts/emptycart.js
@@ -0,0 +1,2 @@
+import header_body from "../components/header_body.js"
+var header=document.getElementById("header").innerHTML=header_body()
